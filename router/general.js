ba79d9b6bc5fa331ed7da6c4471156f3f5ae0e31
Marley <marley.stephens@gmail.com>
2025-10-19 22:01:07 -0400

    First commit

---
+++ router/general.js
@@ -0,0 +1,75 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+
+public_users.post("/register", (req,res) => {
+  //Write your code here
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (isValid(username)) {
+        users.push({"username": username, "password": password});
+        return res.status(200).json({message: "User successfully registered. Now you can login"});
+    } else {
+        return res.status(403).json({message: "User already exists!" });
+    }
+  }
+
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+  //Write your code here
+  res.send(JSON.stringify(books));
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+  //Write your code here
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn]));
+ });
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+  //Write your code here
+  const keys = Object.keys(books);
+  const author = req.params.author;
+  console.log(author)
+  let details = {}
+  for (i=0; i<keys.length; i++){
+    if (author == books[i+1]["author"]){
+        details[i+1] = books[i+1]
+    }
+  }
+  res.send(JSON.stringify(details))
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+  //Write your code here
+  const keys = Object.keys(books);
+  const title = req.params.title;
+  console.log(title)
+  let details = {}
+  for (i=0; i<keys.length; i++){
+    if (title == books[i+1]["title"]){
+        details[i+1] = books[i+1]
+    }
+  }
+  res.send(JSON.stringify(details))
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+  //Write your code here
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn]["reviews"]));
+});
+
+module.exports.general = public_users;
